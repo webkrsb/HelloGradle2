b62d26cc6ae0f52e65a93603e5c750008d11c611
webkrsb <50936953+webkrsb@users.noreply.github.com>
2019-05-22 22:34:37 -0500

    Commit de gradle

---
+++ src/main/java/org/my/main.java
@@ -0,0 +1,10 @@
+package org.my;
+
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
